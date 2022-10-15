1fed006b2cac89167b52bc7de8eb6987275031ff
Sam <96399021+saammsterrss@users.noreply.github.com>
2022-10-15 18:12:11 +1100

    respect

--- src/commands/respect.js
+++ src/commands/respect.js
@@ -6,7 +6,6 @@ module.exports = {
     .setDescription("Pay your respects to two friends of mine that passed away for some daily rewards"),
 
     async execute(interaction) {
-
         const amount = Math.floor(Math.random() * (200 - 1 + 1)) + 1;
         
         return interaction.reply({content: `Thank you for paying your respects, here is ${amount}`});
